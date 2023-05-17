3fcd0e8455463ac9701fc6b0b5e891607641e241
Marwa Mahmoud <marwamahmoud12m@gmail.com>
2023-05-17 11:53:36 +0300

    Delete Tebnabawe.Application directory

--- Tebnabawe.Application/Authentication/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Tebnabawe.Application.Authentication
-{
-    public interface IEmailService
-    {
-         void SendEmail(string toEmail, string subject, string content);
-    }
-}
